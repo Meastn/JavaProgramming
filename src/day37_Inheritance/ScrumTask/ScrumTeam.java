f0dc11b6ce80ef6a635e844a4be550d880b6c9e5
Meastn <av.melihcalikoglu@protonmail.com>
2022-07-28 14:19:06 +0300

    day37_Inheritance

---
+++ src/day37_Inheritance/ScrumTask/ScrumTeam.java
@@ -0,0 +1,12 @@
+package day37_Inheritance.ScrumTask;
+
+public class ScrumTeam {
+
+    public static void main(String[] args) {
+
+        Developer dv1 = new Developer("Bob", 34, 'M',"Mobile Developer", 10011, 100000,"CodeBulls");
+        Developer dv2 = new Developer("James", 36, 'M',"Java Developer", 10012, 11000,"CodeBulls");
+        Tester  tst1= new Tester("Caroline", 27, 'F', "QA", 10013, 90000, "CodeBulls");
+
+    }
+}
